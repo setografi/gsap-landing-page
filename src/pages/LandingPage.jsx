4ce5bee75eab777def1cc6645454be791001293c
setografi <hitonaashbell@gmail.com>
2024-03-21 19:59:52 +0700

    responsive update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -63,30 +63,30 @@ function LandingPage() {
         <nav className=" flex justify-center items-center gap-4 p-4 md:py-8 md:px-4 lg:p-12 opacity-0">
           <img className="w-[120px]" src={LogoImage} alt="logo" />
           <div className="hidden md:flex gap-4 lg:gap-8 lg:mx-16 xl:mx-20">
-            <a className="underline" href="#">
+            <a className="hover:underline" href="#">
               Home
             </a>
-            <a className="underline" href="#">
+            <a className="hover:underline" href="#">
               Shop
             </a>
-            <a className="underline" href="#">
+            <a className="hover:underline" href="#">
               Contact
             </a>
-            <a className="underline" href="#">
+            <a className="hover:underline" href="#">
               Testimonials
             </a>
           </div>
-          <button className="rounded bg-black text-white py-2 px-4 hidden md:inline-block">
+          <button className="bg-black text-white py-2 px-4 hidden md:inline-block rounded transition-all duration-700 hover:bg-gray-300 hover:text-black">
             Let's work together
           </button>
         </nav>
         <header>
-          <div className="first-row flex p-4 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left md:pt-8 lg:pt-16">
-            <p className="bold-text font-bold text-5xl lg:text-6xl xl:text-8xl opacity-0">
+          <div className="first-row flex px-4 pt-2 pb-0 md:p-4 mt-10 md:mt-0 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left md:pt-8 lg:pt-16">
+            <p className="bold-text font-bold text-3xl md:text-5xl lg:text-6xl xl:text-8xl opacity-0">
               The Possibilities
             </p>
 
-            <p className="copy font-medium hidden md:flex md:flex-col opacity-0">
+            <p className="copy font-medium text-base hidden md:flex md:flex-col opacity-0">
               <span>We believe that workspaces</span>
               <span>should be friendly and convenient.</span>
               <span>Here is an invitation into our how</span>
@@ -94,8 +94,8 @@ function LandingPage() {
             </p>
           </div>
 
-          <div className="second-row flex p-4 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left second-row">
-            <p className="bold-text font-bold text-5xl lg:text-6xl xl:text-8xl lg:pl-16 xl:pl-36 opacity-0">
+          <div className="second-row flex px-4 py-2 md:p-4 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left second-row">
+            <p className="bold-text font-bold text-3xl md:text-5xl lg:text-6xl xl:text-8xl lg:pl-16 xl:pl-36 opacity-0">
               Of Workspaces
             </p>
 
@@ -126,20 +126,25 @@ function LandingPage() {
             </p>
           </div>
 
-          <div className="mobile-row flex p-4 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left md:hidden flex-col py-4">
+          <div className="mobile-row flex flex-col py-2 px-4 md:p-4 justify-center md:justify-start items-center gap-4 md:gap-8 lg:gap-12 text-center md:text-left md:hidden">
             <p className="copy font-medium opacity-0">
               <span>We believe that workspaces</span>
+              <br />
               <span>should be friendly and convenient.</span>
+              <br />
               <span>Here is an invitation into our how</span>
+              <br />
               <span>we design workspaces at curved.</span>
             </p>
 
-            <button className="cta opacity-0">Let's work together</button>
+            <button className="cta opacity-0 font-bold md:font-normal cursor-pointer hover:underline">
+              Let's work together
+            </button>
           </div>
         </header>
         <div className="pre-loader absolute z-10">
-          <img src={OfficeImage} alt="an office" />
-          <div className="overlay absolute inset-x-0 bottom-0 top-full bg-black bg-opacity-10 -z-0"></div>
+          <img className="rounded w-full" src={OfficeImage} alt="an office" />
+          <div className="overlay absolute inset-x-0 bottom-0 top-full bg-black bg-opacity-10 rounded -z-0"></div>
         </div>{" "}
       </section>
     </>
